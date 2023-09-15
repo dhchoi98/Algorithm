093d6157a360b3b3045d50091c928288fb0fa55c
DaehanChoi <104803789+dhchoi98@users.noreply.github.com>
2023-09-15 12:57:31 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int num[9];
+    int seq, max;
+
+    for(int i = 0; i < 9; i++){
+        cin >> num[i];
+    }
+    max = num[0];
+    seq = 0;
+    for(int j = 0; j < 9; j++){
+        if(max < num[j]){
+            max = num[j];
+            seq = j;
+        }
+    }
+
+    cout << max << "\n" << seq + 1;
+
+    return 0;
+}
