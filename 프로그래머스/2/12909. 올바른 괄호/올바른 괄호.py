3febc1a93685222b2e281f3307db90d805a6eb7f
Daehan Choi <chleogks9808@naver.com>
2025-02-27 11:43:04 +0900

    [level 2] Title: 올바른 괄호, Time: 7.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,10 @@
 def solution(s):
-    answer = True
-    
-    pattern = { ")" : "("}
     stack = []
     
-    for c in s:
-        if c == "(":   # 왼쪽 괄호
-            stack.append(c)
-        elif not stack or pattern[c] != stack.pop():
-            return False
-        
-    return len(stack) == 0
+    for char in s:
+        if char == "(": # 왼쪽 괄호면 스택에 삽입
+            stack.append(char)
+        elif not stack or "(" != stack.pop() : # 오른쪽 괄호라면 닫힌 괄호인지 확인
+            return False # 문자열이 끝나지 않았는데 스택이 비어있다는 것은 오른쪽 괄호가 더 많다는 것
+    
+    return len(stack) == 0 # 다 끝났을 때 비어있다는 것은 True 인 것
