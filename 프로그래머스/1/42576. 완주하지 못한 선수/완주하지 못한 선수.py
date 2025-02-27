ab3212cf8a0be680ddb6c65c4865d00aed1c15ff
Daehan Choi <chleogks9808@naver.com>
2025-02-27 13:31:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.48 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,22 @@
-from collections import Counter
-
 def solution(participant, completion):
-    # ww
-    participant.sort()
-    completion.sort()
+    hashDict = {}
+    sumHash = 0
     
-    part = Counter(participant)
-    comp = Counter(completion)
-    sets = set(participant)
+    # 1. Hash : Participant의 dictionary 만들기
+    # 2. Participant의 sum(hash) 구하기
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
     
-    for key, value in part.items():
-        if comp[key] != value:
-            return key
-        
+    # 3. completion의 sum(hash) 빼기
+    for comp in completion:
+        sumHash -= hash(comp)
+    
+    # 4. 남은 값이 완주하지 못한 선수의 hash 값이 된다
+
+    return hashDict[sumHash]
+
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"]
+, ["josipa", "filipa", "marina", "nikola"]))
+
         
