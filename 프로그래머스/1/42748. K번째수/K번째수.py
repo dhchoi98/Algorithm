d7be61364c4ec49059157d4d660f8e864ba90662
Daehan Choi <chleogks9808@naver.com>
2025-03-02 11:35:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for i, j, k in commands:
+        slice = array[i-1:j]
+        slice.sort()
+        answer.append(slice[k - 1])
+    
+    
+    return answer
