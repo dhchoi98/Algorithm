177b63cfe7c37d6f408d1b3d344b7aa5f80308de
Daehan Choi <chleogks9808@naver.com>
2025-05-30 15:45:58 +0900

    [Silver III] Title: Four Squares, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,16 +1,30 @@
-n = int(input())
-arr = [0 if i**0.5%1 else 1 for i in range(n+1)] # 제곱수는 1로 저장
+import sys
 
-min_ = 4
-for i in range(int(n**0.5), 0, -1):
-    if arr[n] : # n이 제곱수일 경우
-        min_=1
-        break
-    elif arr[n-i**2] : # 나머지가 제곱수일 경우
-        min_=2
-        break
-    else:
-        for j in range(int((n-i**2)**0.5), 0, -1):
-            if arr[(n-i**2)-j**2]: # 제곱수를 한번 더 뺀 나머지가 제곱수일 경우
-                min_=3
-print(min_)
+n = int(sys.stdin.readline())
+
+# 라그랑주의 네 제곱수 정리를 활용한 최적화
+# 모든 자연수는 최대 4개의 제곱수의 합으로 표현 가능
+
+# n이 완전제곱수인지 확인
+i = 1
+while i * i <= n:
+    if i * i == n:
+        print(1)
+        sys.exit()
+    i += 1
+
+# n이 두 제곱수의 합인지 확인
+for i in range(1, int(n**0.5) + 1):
+    j = int((n - i*i)**0.5)
+    if i*i + j*j == n:
+        print(2)
+        sys.exit()
+
+# n이 4^a(8b+7) 형태인지 확인 (라그랑주 정리에 의해 이 경우만 4개 필요)
+while n % 4 == 0:
+    n //= 4
+
+if n % 8 == 7:
+    print(4)
+else:
+    print(3)
