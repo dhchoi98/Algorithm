3697e231bd42e4d6102825368bd58d4dd0263c8c
Daehan Choi <chleogks9808@naver.com>
2025-08-17 23:49:23 +0900

    [Silver III] Title: 소수 구하기, Time: 5936 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,21 +1,11 @@
-import sys
-import math
+N, M = map(int, input().split())
 
-m, n = map(int, sys.stdin.readline().strip().split())
-
-prime_number = []
-
-for i in range(m, n + 1):
-    if i < 2:
+for i in range(N, M+1):
+    if i == 1: # 1은 통과
         continue
-    root_n = math.isqrt(i)
-    is_prime = True
-    for j in range(2, root_n + 1):
-        if i % j == 0:
-            is_prime = False
+        
+    for j in range(2, int(i**0.5) + 1): # 2 ~ 제곱근 사이 값 확인
+        if i % j == 0: # 나누어 떨어진다면 그대로 종료 (else문 실행 X)
             break
-    if is_prime:
-        prime_number.append(i)
-
-for i in prime_number:
-    print(i)
+    else: # for문이 잘 실행이 되었다면 print(i) 출력
+        print(i)
