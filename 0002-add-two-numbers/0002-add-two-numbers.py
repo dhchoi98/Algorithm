ff4077d20a7f446c67117a1e130a9ac800a740df
Daehan Choi <chleogks9808@naver.com>
2025-09-13 19:11:57 +0900

    Time: 7 ms (31.46%), Space: 17.6 MB (99.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,40 +6,23 @@
 
 
 class Solution:
-            # 연결 리스트 뒤집기
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None
-
-        while node:
-            next, node.next = node.next, prev
-            prev, node = node, next
-            
-        return prev
-
-        # 연결 리스트를 파이썬 리스트로 변환
-    def toList(self, node: ListNode) -> list:
-        list_1: List = []
-        while node:
-            list_1.append(node.val)
-            node = node.next
-        return list_1
-        # 파이썬 리스트를 연결 리스트로 변환
-
-    def toReversedLinkedList(self, result: ListNode) -> ListNode:
-        prev: ListNode = None
-        for r in result:
-            node = ListNode(r)
-            node.next = prev
-            prev = node
-
-        return node
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = self.toList(self.reverseList(l1))
-        b = self.toList(self.reverseList(l2))
+        root = head = ListNode(0)
 
-        resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
-        
-        digits = [int(ch) for ch in str(resultStr)]
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            # 두 입력값의 합 계산
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
 
-        return self.toReversedLinkedList(digits)
+            # 몫 (자리 올림수)과 나머지(값) 계산
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
