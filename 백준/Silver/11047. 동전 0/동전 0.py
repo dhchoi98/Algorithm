7ecb25b6a1110892413eb065ffc61e0260ef69a0
Daehan Choi <chleogks9808@naver.com>
2025-09-29 19:10:02 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,18 +1,20 @@
 import sys
-# 그리디 알고리즘
 
-N, K = map(int, sys.stdin.readline().strip().split())
+
+# 동전의 종류, 만들어야 할 가치 합
+n, k = map(int, sys.stdin.readline().split())
 
 coins = []
-for _ in range(N):
-    coins.append(int(sys.stdin.readline().strip()))
+# 동전의 가치 배열 coins(i)
+for _ in range(n):
+    coins.append(int(sys.stdin.readline()))
 coins.sort(reverse=True)
 
-answer = 0
+ans = 0
 for coin in coins:
-    if K >= coin:
-        answer += K // coin
-        K %= coin
-        if K <= 0:
-            break
-print(answer) 
+    if k == 0: break
+    ans += k // coin
+    k = k % coin
+
+
+print(ans)
