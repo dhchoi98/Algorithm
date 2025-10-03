4f9cc087075dd5cad46b6ab8b938caa16898c844
Daehan Choi <chleogks9808@naver.com>
2025-10-03 18:36:06 +0900

    Time: 0 ms (100%), Space: 18.1 MB (20.82%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,32 @@
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()   # 미리 output 채우기
+        return self.output.pop()
+
+    def peek(self) -> int:
+        # output이 비어있을 때만 input에서 옮김
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        return not self.input and not self.output
+
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
