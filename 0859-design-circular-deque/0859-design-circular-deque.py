e8f410bd19cfc96684e2fd99feff46be120321f6
Daehan Choi <chleogks9808@naver.com>
2025-10-05 19:23:31 +0900

    Time: 13 ms (16.81%), Space: 19 MB (5.39%) - LeetHub

---
+++ 0859-design-circular-deque/0859-design-circular-deque.py
@@ -0,0 +1,68 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.head, self.tail = ListNode(None), ListNode(None)
+        self.k, self.len = k, 0
+        self.head.right, self.tail.left = self.tail, self.head
+
+    def add_node(self, node: ListNode, new: ListNode):
+        n = node.right
+        node.right = new
+        new.left, new.right = node, n
+        n.left = new
+
+    def del_node(self, node: ListNode):
+        n = node.right.right
+        node.right, n.left = n, node
+
+    def insertFront(self, value: int) -> bool:
+        if self.len == self.k:
+            return False
+        self.len += 1
+        self.add_node(self.head, ListNode(value)) 
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.len == self.k:
+            return False
+        self.len += 1
+        self.add_node(self.tail.left, ListNode(value))
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self.del_node(self.head)
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self.del_node(self.tail.left.left)
+        return True
+
+    def getFront(self) -> int:
+        return self.head.right.val if self.len else -1
+
+    def getRear(self) -> int:
+        return self.tail.left.val if self.len else -1
+
+    def isEmpty(self) -> bool:
+        return self.len == 0        
+
+    def isFull(self) -> bool:
+        return self.len == self.k
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
