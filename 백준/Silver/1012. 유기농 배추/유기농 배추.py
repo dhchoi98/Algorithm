4c0eb397031df21a1b7635bc81d28ac3079d538c
Daehan Choi <chleogks9808@naver.com>
2025-10-20 23:18:47 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32808 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,32 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 
 T = int(sys.stdin.readline())
-
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    visited[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and field[nx][ny] == 1 and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                q.append((nx, ny))
-
+def dfs(i: int, j: int, cabbage_field: list[list[int]]):
+    cabbage_field[i][j] = 0
+    for k in range(4):
+        nx = i + dx[k]
+        ny = j + dy[k]
+        if 0 <= nx < len(cabbage_field) and 0 <= ny < len(cabbage_field[0]) \
+           and cabbage_field[nx][ny] == 1:
+            dfs(nx, ny, cabbage_field)
 
 for _ in range(T):
-    M, N, K = map(int, sys.stdin.readline().split())
+    M, N, K = map(int, sys.stdin.readline().split())  # M: 가로(열), N: 세로(행)
+    cabbage_field = [[0] * M for _ in range(N)]       # [행][열] = [N][M]
 
-    field = [[0 for col in range(M)] for row in range(N)]
-    visited = [[0 for col in range(M)] for row in range(N )]
+    for _ in range(K):
+        X, Y = map(int, sys.stdin.readline().split()) # X: 열, Y: 행
+        cabbage_field[Y][X] = 1                       # [행][열]
 
-    for i in range(K):
-        X, Y = map(int, sys.stdin.readline().split())
-        field[Y][X] = 1
+    answer = 0
+    for i in range(N):          # 행
+        for j in range(M):      # 열
+            if cabbage_field[i][j] == 1:
+                answer += 1
+                dfs(i, j, cabbage_field)
 
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if field[i][j] == 1 and visited[i][j] == 0:
-                cnt += 1
-                bfs(i, j)
-
-    print(cnt)
+    print(answer)
