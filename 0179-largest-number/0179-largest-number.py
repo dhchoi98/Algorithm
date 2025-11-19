6d223301877deaca607d19a7999d833f7f1f72ef
dhchoi98 <chleogks9808@naver.com>
2025-11-19 20:26:04 +0900

    Time: 5 ms (16.9%), Space: 17.8 MB (59.93%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,17 +1,19 @@
-class Solution:
-        # 문제에 적합한 비교 함수
-        @staticmethod
-        def to_swap(n1: int, n2: int) -> bool:
-            return str(n1) + str(n2) < str(n2) + str(n1)
+from functools import cmp_to_key
 
-        # 삽입 정렬 구현
-        def largestNumber(self, nums: List[int]) -> str:
-            i = 1
-            while i < len(nums):
-                j = i
-                while j > 0 and self.to_swap(nums[j - 1], nums[j]):
-                    nums[j], nums[j - 1] = nums[j - 1], nums[j]
-                    j -= 1
-                i += 1
-            
-            return str(int(''.join(map(str, nums))))
+class Solution:
+    def largestNumber(self, nums):
+        nums = list(map(str, nums))
+
+        def compare(a, b):
+            if a + b > b + a:
+                return -1
+            elif a + b < b + a:
+                return 1
+            else:
+                return 0
+
+        nums.sort(key=cmp_to_key(compare))
+
+        result = ''.join(nums)
+
+        return '0' if result[0] == '0' else result
